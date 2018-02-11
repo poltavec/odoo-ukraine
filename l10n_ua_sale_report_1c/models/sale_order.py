b7e7188e0531c95662de0d898e1e4173895afa58
edehtiar <yevhenii.dehtiar@gmail.com>
2018-02-12 00:56:09 +0200

    unicode warning fix

--- l10n_ua_sale_report_1c/models/sale_order.py
+++ l10n_ua_sale_report_1c/models/sale_order.py
@@ -6,4 +6,4 @@ class SaleOrder(models.Model):
 
     _inherit = "sale.order"
 
-    location_signed_in = fields.Char(string=_(u'Місце складання'))
+    location_signed_in = fields.Char(string=_('Location signed'))
